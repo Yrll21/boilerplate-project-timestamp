26f84ed64fe330cb49ef83f37de25e78e7521956
Ivan Yrll Labadia <ivanyrlllabadia@gmail.com>
2024-04-23 09:32:25 +0000

    Finished freecodecamp timestamp microservice

--- index.js
+++ index.js
@@ -24,7 +24,72 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function(req,res) {
+  console.log("empty")
+  var unix = Math.floor(new Date().getTime())
+  console.log(unix)
+  var initialDate = new Date()
+  var utc = initialDate.toUTCString()
+  console.log(utc)
+  var data = {
+    unix: unix,
+    utc: utc
+  }
+  res.json(data)
+})
 
+//project features start here
+app.get("/api/:date", function(req,res) {
+  const inputDate = req.params.date
+  // if input is empty
+  if (inputDate == "" || inputDate == NaN) {
+    console.log(empty)
+
+  } else {
+    const isValidDate = new Date(inputDate)
+    const isValidTimeStamp = /^[0-9]+$/.test(inputDate)
+    // console.log("isValidDate?", isValidDate)
+    // console.log("isValidTimeStamp?", isValidTimeStamp)
+
+    // Input is invalid
+    if (isValidDate == "Invalid Date" && !isValidTimeStamp) {
+      res.send({error: "Invalid Date"})
+    }
+
+    // If input is a valid date
+    if (isValidDate != "Invalid Date" && !isValidTimeStamp) {
+      // console.log("input is a valid date")
+      var unix = Math.floor(new Date(inputDate).getTime())
+      console.log(unix)
+      var initialDate = new Date(inputDate)
+      var utc = initialDate.toUTCString()
+      console.log(utc)
+      var data = {
+        unix: unix,
+        utc: utc
+      }
+      res.json(data)
+    }
+
+    // If input is a valid timestamp
+    if (isValidDate == "Invalid Date" && isValidTimeStamp) {
+      // console.log("Input is timestamp")
+      var unix = parseInt(inputDate)
+      var initialDate = new Date(unix)
+      var utc = initialDate.toUTCString()
+      console.log(utc)
+      var data = {
+        unix: unix,
+        utc: utc
+      }
+      res.json(data)
+    }
+  }
+  
+
+
+
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
